03d38c3fb2f952f2742f854b4a99d5d59d3c3e8f
pferdefleisch <aaron@aaroncruz.com>
2010-09-20 11:14:57 +0200

    added ajax function

--- zepto.js
+++ zepto.js
@@ -1,6 +1,6 @@
 var $ = function(selector){
   return { dom: Array.prototype.slice.apply(document.querySelectorAll(selector)),
-    css: $.css, html: $.html };
+    css: $.css, html: $.html, ajax: $.ajax };
 }
 
 $.html = function(html){
@@ -9,4 +9,41 @@ $.html = function(html){
 
 $.css = function(style){
   this.dom.forEach(function(el){ el.style.cssText += ';'+style }); return this;
+}
+
+$.ajax = function(settings){
+	if(!settings.url || !settings.success) return;
+	var async = settings.async || true,
+		data = settings.data || '',
+		type = settings.type || 'GET',
+		success = settings.success,
+		url = settings.url,
+		request;
+	
+	if(typeof data === 'object'){
+		var dataArray = [],
+			key;
+		for(key in data){
+			dataArray.push(key + '=' + data[key]); 
+		}
+		data = '?' + dataArray.join('&');
+	}
+	
+	try{
+		request = new XMLHttpRequest();
+	}catch(e){}
+	
+	url = url + data;
+	request.onreadystatechange = stateChange;
+	request.open(type.toUpperCase(),url,async);
+	request.send(null);
+	function stateChange(){
+		if(request.readyState === 4){
+			if(request.status === 200){
+				success(request.responseText);
+			}else{
+				return false;
+			}
+		}
+	}
 }
