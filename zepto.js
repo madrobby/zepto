2969571112378a318056e97fbd0fcb9e2f92e075
mstffn <marco@royal-design.com>
2010-09-20 19:42:35 +0200

    added append and prepend..

--- zepto.js
+++ zepto.js
@@ -6,6 +6,14 @@ $.html = function(html){
   $.dom.forEach(function(el){ el.innerHTML = html }); return $;
 }
 
+$.append = function(html){
+  $.dom.forEach(function(el){ el.insertAdjacentHTML('beforeEnd',html) }); return $;
+}
+
+$.prepend = function(html){
+  $.dom.forEach(function(el){ el.insertAdjacentHTML('afterBegin',html) }); return $;
+}
+
 $.css = function(style){
   $.dom.forEach(function(el){ el.style.cssText += ';'+style }); return $;
 }
