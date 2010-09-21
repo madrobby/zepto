f2b114ca2e18f38fe32c2e214a10d789b73f9a82
Thomas Fuchs <thomas@fesch.at>
2010-09-21 11:01:59 +0200

    add .fn namespace. add popular ajax calls.

--- zepto.js
+++ zepto.js
@@ -1,21 +1,39 @@
-var $ = function(_){
+function $(_){
   if(typeof _ == 'function') $.dom.forEach(_)
     else $.dom = [].slice.apply(document.querySelectorAll(_));
-  return $;
+  return $.fn;
 }
 
-$.html = function(html){
-  return $(function(el){ el.innerHTML = html });
-}
+$.fn = {
+  get: function(idx){ return idx === undefined ? $.dom : $.dom[idx] },
+  html: function(html){
+    return $(function(el){ el.innerHTML = html });
+  },
+  append: function(html){
+    return $(function(el){ el.insertAdjacentHTML('beforeEnd',html) });
+  },
+  prepend: function(html){
+    return $(function(el){ el.insertAdjacentHTML('afterBegin',html) });
+  },
+  css: function(style){
+    return $(function(el){ el.style.cssText += ';'+style });
+  }
+};
 
-$.append = function(html){
-  return $(function(el){ el.insertAdjacentHTML('beforeEnd',html) });
-}
-
-$.prepend = function(html){
-  return $(function(el){ el.insertAdjacentHTML('afterBegin',html) });
-}
-
-$.css = function(style){
-  return $(function(el){ el.style.cssText += ';'+style });
-}
+(function(){
+  function ajax(method, url, success){
+    var r = new XMLHttpRequest();
+    r.onreadystatechange = function(){
+      if(r.readyState==4 && r.status==200) 
+        success(r.responseText);
+    };
+    r.open(method,url,true);
+    r.send(null);
+  }
+  
+  $.get = function(url, success){ ajax('GET', url, success); };
+  $.post = function(url, success){ ajax('POST', url, success); };
+  $.getJSON = function(url, success){ 
+    $.get(url, function(json){ success(JSON.parse(json)) });
+  };
+})();
