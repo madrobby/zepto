b124ebb826e6d7b6f8ab3e0e6683ed8d7f61e425
Thomas Fuchs <thomas@fesch.at>
2010-09-22 19:54:16 +0200

    add before/after. slight refactor. extend tests

--- zepto.js
+++ zepto.js
@@ -1,44 +1,42 @@
-(function(slice) {
-  var $ = function(_){
+var $ = (function() {
+  var slice = [].slice, k, 
+    ADJ_OPS = {append: 'beforeEnd', prepend: 'afterBegin', before: 'beforeBegin', after: 'afterEnd'};
+  
+  function $(_){
     if(typeof _ == 'function') $.dom.forEach(_)
-    else { 
-      $._   = _;
-      $.dom = slice.call(document.querySelectorAll(_));
-    }
-    return $.fn;
-  };
-
-
-$.fn = {
-  get: function(idx){ return idx === undefined ? $.dom : $.dom[idx] },
-  html: function(html){
-    return $(function(el){ el.innerHTML = html });
-  },
-  append: function(html){
-    return $(function(el){ el.insertAdjacentHTML('beforeEnd',html) });
-  },
-  prepend: function(html){
-    return $(function(el){ el.insertAdjacentHTML('afterBegin',html) });
-  },
-  css: function(style){
-    return $(function(el){ el.style.cssText += ';'+style });
-  },
-  anim: function(transform, opacity, dur){
-    return $.fn.css('-webkit-transition:all '+(dur||0.5)+'s;'+
-      '-webkit-transform:'+transform+';opacity:'+(opacity===0?0:opacity||1));
-  },
-  live: function(event, callback){
-    var selector = $._;
-    document.body.addEventListener(event, function(event){
-      var target = event.target, nodes = slice.call(document.querySelectorAll(selector));
-      while(target && nodes.indexOf(target)<0) target = target.parentNode;
-      if(target && !(target===document)) callback.call(target, event);
-    }, false);
+    else
+      $.dom = slice.call(document.querySelectorAll($._ = _));
     return $.fn;
   }
-};
 
-
+  $.fn = {
+    get: function(idx){ return idx === undefined ? $.dom : $.dom[idx] },
+    html: function(html){
+      return $(function(el){ el.innerHTML = html });
+    },
+    css: function(style){
+      return $(function(el){ el.style.cssText += ';'+style });
+    },
+    anim: function(transform, opacity, dur){
+      return $.fn.css('-webkit-transition:all '+(dur||0.5)+'s;'+
+        '-webkit-transform:'+transform+';opacity:'+(opacity===0?0:opacity||1));
+    },
+    live: function(event, callback){
+      var selector = $._;
+      document.body.addEventListener(event, function(event){
+        var target = event.target, nodes = slice.call(document.querySelectorAll(selector));
+        while(target && nodes.indexOf(target)<0) target = target.parentNode;
+        if(target && !(target===document)) callback.call(target, event);
+      }, false);
+      return $.fn;
+    }
+  };
+  
+  for(k in ADJ_OPS)
+    $.fn[k] = (function(op){ 
+      return function(html){ return $(function(el){ console.log(op); el.insertAdjacentHTML(op,html) }) };
+    })(ADJ_OPS[k]);
+  
   function ajax(method, url, success){
     var r = new XMLHttpRequest();
     r.onreadystatechange = function(){
@@ -54,5 +52,6 @@ $.fn = {
   $.getJSON = function(url, success){ 
     $.get(url, function(json){ success(JSON.parse(json)) });
   };
-  this.$ = $;
-})([].slice);
+
+  return $;
+})();
