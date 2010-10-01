5232fb9fe51baba20a0482b3cbe82ce5f1e6671c
Thomas Fuchs <thomas@fesch.at>
2010-10-01 12:48:27 +0200

    cleanup

--- zepto.js
+++ zepto.js
@@ -24,7 +24,7 @@ var $ = (function(d) {
     },
     attr: function(name,value){
       return (typeof name == 'string' && value === void 0) ? (this.dom.length>0 ? this.dom[0].getAttribute(name) || undefined : null) :
-        this(function(el){ 
+        this(function(el){
           if (typeof name == 'object') for(k in name) el.setAttribute(k, name[k])
           else el.setAttribute(name,value);
         });
@@ -33,7 +33,7 @@ var $ = (function(d) {
       return this(function(el){ el.style.cssText += ';'+style });
     },
     index: function(target){
-      return [].indexOf.call(this.dom, $(target).get(0));    
+      return [].indexOf.call(this.dom, $(target).get(0));
     },
     anim: function(transform, opacity, dur){
       return this.css('-webkit-transition:all '+(dur||0.5)+'s;'+
@@ -59,14 +59,14 @@ var $ = (function(d) {
       });
     }
   };
-  
+
   for(k in ADJ_OPS)
     $.fn[k] = (function(op){
       return function(html){ return this(function(el){
         el['insertAdjacent' + (html instanceof Element ? 'Element' : 'HTML')](op,html)
       })};
     })(ADJ_OPS[k]);
-  
+
   function ajax(method, url, success){
     var r = new XMLHttpRequest();
     r.onreadystatechange = function(){
@@ -76,10 +76,10 @@ var $ = (function(d) {
     r.open(method,url,true);
     r.send(null);
   }
-  
+
   $.get = function(url, success){ ajax('GET', url, success); };
   $.post = function(url, success){ ajax('POST', url, success); };
-  $.getJSON = function(url, success){ 
+  $.getJSON = function(url, success){
     $.get(url, function(json){ success(JSON.parse(json)) });
   };
 
