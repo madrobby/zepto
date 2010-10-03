137cbf6cd96d117e9a3157769ce9999ee00d59ca
Thomas Fuchs <thomas@fesch.at>
2010-10-03 15:58:42 +0200

    fire doubletap event only when finger is lifted

--- zepto.js
+++ zepto.js
@@ -110,18 +110,19 @@ var $ = (function(d) {
   d.ontouchstart = function(e) {
     var now = Date.now(), delta = now-(touch.last || now);
     touch.target = e.touches[0].target;
-    if(touchTimeout) clearTimeout(touchTimeout);
+    touchTimeout && clearTimeout(touchTimeout);
     touch.x1 = e.touches[0].pageX;
-    if (delta > 0 && delta <= 250) {
-      dispatch('doubleTap', touch.target);
-      touch = {};
-    } else touch.last = now;
+    if (delta > 0 && delta <= 250) touch.isDoubleTap = true;
+    touch.last = now;
   }
 
   d.ontouchmove = function(e) { touch.x2 = e.touches[0].pageX }
 
   d.ontouchend = function(e) {
-    if (touch.x2 > 0) {
+    if (touch.isDoubleTap) {
+      dispatch('doubleTap', touch.target);
+      touch = {};
+    } else if (touch.x2 > 0) {
       touch.x1 - touch.x2 > 30 && dispatch('swipe', touch.target);
       touch.x1 - touch.x2 < -30 && dispatch('swipe', touch.target);
       touch.x1 = touch.x2 = touch.last = 0;
