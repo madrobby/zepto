e74a836526a278ba6e5c64dd441911629c19a9e2
BradBirdsall <brad@primestudiosllc.com>
2010-10-12 20:50:25 -0400

    jQuery-like load functionality with built-in response html parsing by selector - a peice I will be using as I implement Zepto and I assume others might need it aslo

--- src/ajax.js
+++ src/ajax.js
@@ -15,4 +15,23 @@
   $.getJSON = function(url, success){
     $.get(url, function(json){ success(JSON.parse(json)) });
   };
+  
+  $.fn.load = function(url, selector, success) {   // some functionality and methods inspired from jQuery source (MIT Licensed)
+    var that = this;
+    if (!this.length) return this;
+    if (Object.prototype.toString.call(selector) === "[object Function]") { // if 2nd arg exists and is function
+      success = selector;
+      selector = null;
+    };
+    $.get(url, function(data) {
+      var resp = data.replace(/<script(.|\s)*?\/script>/gi, '');
+      that.html(
+        selector ?
+          $(document.createElement('div')).html(resp).find(selector).html()
+          : resp );
+      if (success) success();
+    });
+    return this;
+  };
+  
 })(Zepto);
