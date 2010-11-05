5f1356a76a41eb7017898f01ebd2210fe38777e8
tim <tim.hatch@o2.co.uk>
2010-11-05 04:53:45 -0700

    $.post() fails in v0.1.1 implementation - r.send(null) causes the XHR request to be treated as a 'GET' request regardless as to what method is specified in ajax(). Fixed, perhaps crudely, by adding a data parameter to $.post() and ajax() which is set to null for a 'GET' request. Also added a second header request specifically for 'POST' requests

--- src/ajax.js
+++ src/ajax.js
@@ -1,29 +1,21 @@
 (function($){
-  function ajax(method, url, success){
+  function ajax(method, url, data, success){
     var r = new XMLHttpRequest();
-    r.onreadystatechange = function(){
-      if(r.readyState==4 && (r.status==200 || r.status==0))
-        success(r.responseText);
-    };
+    r.onreadystatechange = function(){ if(r.readyState==4 && (r.status==200 || r.status==0)) success(r.responseText) };
     r.open(method,url,true);
     r.setRequestHeader('X-Requested-With','XMLHttpRequest');
-    r.send(null);
+    if (method=='POST') r.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+    r.send(data);
   }
-
-  $.get = function(url, success){ ajax('GET', url, success); };
-  $.post = function(url, success){ ajax('POST', url, success); };
-  $.getJSON = function(url, success){
-    $.get(url, function(json){ success(JSON.parse(json)) });
-  };
-  
+  $.get = function(url, success){ ajax('GET', url, null, success); };
+  $.getJSON = function(url, success){ $.get(url, function(json){ success(JSON.parse(json)) }) };
+  $.post = function(url, data, success){ ajax('POST', url, data, success); };
   $.fn.load = function(url, success){
     var self = this, parts = url.split(/\s/), selector;
     if(!this.length) return this;
     if(parts.length>1) url = parts[0], selector = parts[1];
     $.get(url, function(response){
-      self.html(selector ?
-        $(document.createElement('div')).html(response).find(selector).html()
-        : response);
+      self.html(selector ? $(document.createElement('div')).html(response).find(selector).html() : response);
       success && success();
     });
     return this;
