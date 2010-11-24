f1786462e541b30215457e78386ab863d7bc7c32
Jakub Nesetril <jakub@gooddata.com>
2010-11-24 17:17:55 +0100

    Made $.post jQuery-compatible and added tests for it

--- src/ajax.js
+++ src/ajax.js
@@ -1,19 +1,25 @@
 (function($){
-  function ajax(method, url, success, data){
+  function ajax(method, url, success, data, type){
     data = data || null;
-    if (data instanceof Object) data = JSON.stringify(data);
     var r = new XMLHttpRequest();
-    r.onreadystatechange = function(){
-      if(r.readyState==4 && (r.status==200 || r.status==0))
-        success(r.responseText);
-    };
+    if (success instanceof Function) {
+      r.onreadystatechange = function(){
+        if(r.readyState==4 && (r.status==200 || r.status==0))
+          success(r.responseText);
+      };
+    }
     r.open(method,url,true);
+    if (type) r.setRequestHeader("Accept", type );
+    if (data instanceof Object) data = JSON.stringify(data), r.setRequestHeader('Content-Type','application/json');
     r.setRequestHeader('X-Requested-With','XMLHttpRequest');
     r.send(data);
   }
 
   $.get = function(url, success){ ajax('GET', url, success); };
-  $.post = function(url, success, data){ ajax('POST', url, success, data); };
+  $.post = function(url, data, success, type){
+    if (data instanceof Function) type = type || success, success = data, data = null;
+    ajax('POST', url, success, data, type);
+  };
   $.getJSON = function(url, success){
     $.get(url, function(json){ success(JSON.parse(json)) });
   };
