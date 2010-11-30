36f62c01855cacc57a0175258dd6f868d5cd3f13
OiNutter <willmckenzie@oinutter.co.uk>
2010-11-30 09:46:22 +0000

    Updated anim function to include support for mozilla transforms and transitions.  Enables zepto to create animations on Fennec

--- src/fx.js
+++ src/fx.js
@@ -2,7 +2,7 @@
   $.fn.anim = function(props, dur, ease){
     var transforms = [], opacity, k;
     for (k in props) k === 'opacity' ? opacity=props[k] : transforms.push(k+'('+props[k]+')');
-    return this.css({ '-webkit-transition': 'all '+(dur||0.5)+'s '+(ease||''),
-      '-webkit-transform': transforms.join(' '), opacity: opacity });
+    return this.css({ '-webkit-transition': 'all '+(dur||0.5)+'s '+(ease||''), '-moz-transition': 'all '+(dur||0.5)+'s '+(ease||''),
+      '-webkit-transform': transforms.join(' '), '-moz-transform': transforms.join(' '), opacity: opacity });
   }
 })(Zepto);
