4eab3639976a79ca51ea01f30f0de990f11c5810
Sam Stephenson <sam@37signals.com>
2010-12-08 23:06:39 -0600

    Add $.fn.undelegate and $.fn.die

--- src/event.js
+++ src/event.js
@@ -3,57 +3,82 @@
   function zid(element) {
     return element._zid || (element._zid = _zid++);
   }
-  function find(element, event, fn) {
-    return handlers[zid(element)].filter(function(handler) {
-      return handler && (!event || handler.event === event)
-        && (!fn || handler.fn === fn);
+  function findHandlers(element, event, fn, selector) {
+    return (handlers[zid(element)] || []).filter(function(handler) {
+      return handler
+        && (!event || handler.ev == event)
+        && (!fn || handler.fn == fn)
+        && (!selector || handler.sel == selector);
+    });
+  }
+
+  function add(element, events, fn, selector, delegate){
+    var id = zid(element);
+    handlers[id] || (handlers[id] = []);
+    events.split(/\s/).forEach(function(event){
+      var handler = {ev: event, fn: fn, sel: selector, del: delegate, i: handlers[id].length};
+      handlers[id].push(handler);
+      element.addEventListener(event, delegate || fn, false);
+    });
+  }
+  function remove(element, events, fn, selector){
+    var id = zid(element);
+    (events || '').split(/\s/).forEach(function(event){
+      findHandlers(element, event, fn, selector).forEach(function(handler){
+        delete handlers[id][handler.i];
+        element.removeEventListener(handler.ev, handler.del || handler.fn, false);
+      });
     });
   }
 
   $.event = {
     add: function(element, events, fn){
-      var id = zid(element);
-      handlers[id] || (handlers[id] = []);
-      events.split(/\s/).forEach(function(event){
-        var handler = {event: event, fn: fn, i: handlers[id].length};
-        handlers[id].push(handler);
-        element.addEventListener(event, fn, false);
-      });
+      add(element, events, fn);
     },
     remove: function(element, events, fn){
-      var id = zid(element);
-      (events || '').split(/\s/).forEach(function(event){
-        find(element, event, fn).forEach(function(handler){
-          delete handlers[id][handler.i];
-          element.removeEventListener(handler.event, handler.fn, false);
-        });
-      });
+      remove(element, events, fn);
     }
   };
+
   $.fn.bind = function(event, callback){
     return this.each(function(element){
-      $.event.add(element, event, callback);
+      add(element, event, callback);
     });
   };
   $.fn.unbind = function(event, callback){
     return this.each(function(element){
-      $.event.remove(element, event, callback);
+      remove(element, event, callback);
     });
   };
+
   $.fn.delegate = function(selector, event, callback){
     return this.each(function(element){
-      $.event.add(element, event, function(e){
+      var delegate = function(e){
         var target = e.target, nodes = $$(element, selector);
         var proxy = {originalEvent: e, liveFired: element};
         while (target && nodes.indexOf(target) < 0) target = target.parentNode;
         $.extend(proxy, e).currentTarget = target;
-        if (target && !(target === element) && !(target === document)) callback.call(target, proxy);
-      });
+        if (target && !(target === element) && !(target === document))
+          callback.call(target, proxy);
+      };
+      add(element, event, callback, selector, delegate);
     });
   };
+  $.fn.undelegate = function(selector, event, callback){
+    return this.each(function(element){
+      remove(element, event, callback, selector);
+    });
+  }
+
   $.fn.live = function(event, callback){
-    $(document.body).delegate(this.selector, event, callback); return this;
+    $(document.body).delegate(this.selector, event, callback);
+    return this;
   };
+  $.fn.die = function(event, callback){
+    $(document.body).undelegate(this.selector, event, callback);
+    return this;
+  };
+
   $.fn.trigger = function(event){
     return this.each(function(element){
       var e = document.createEvent('Events');
