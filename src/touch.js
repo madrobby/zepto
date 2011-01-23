fd27542b3017b3ce3ee500fd0bce8068b14a72d2
Matt <Matt@Matt6.lan>
2011-01-23 17:34:34 +0000

    Added "swipeUp" and "swipeDown" events and updated test page

--- src/touch.js
+++ src/touch.js
@@ -11,18 +11,22 @@
       touch.target = parentIfText(e.touches[0].target);
       touchTimeout && clearTimeout(touchTimeout);
       touch.x1 = e.touches[0].pageX;
+      touch.y1 = e.touches[0].pageY;
       if (delta > 0 && delta <= 250) touch.isDoubleTap = true;
       touch.last = now;
     }).bind('touchmove', function(e){
       touch.x2 = e.touches[0].pageX
+      touch.y2 = e.touches[0].pageY
     }).bind('touchend', function(e){
       if (touch.isDoubleTap) {
         $(touch.target).trigger('doubleTap');
         touch = {};
-      } else if (touch.x2 > 0) {
+      } else if (touch.x2 > 0 || touch.y2 > 0) {
         Math.abs(touch.x1 - touch.x2) > 30 && $(touch.target).trigger('swipe') &&
-          $(touch.target).trigger('swipe' + (touch.x1 - touch.x2 > 0 ? 'Left' : 'Right'));
-        touch.x1 = touch.x2 = touch.last = 0;
+        $(touch.target).trigger('swipe' + (touch.x1 - touch.x2 > 0 ? 'Left' : 'Right'));
+        Math.abs(touch.y1 - touch.y2) > 30 && $(touch.target).trigger('swipe') &&
+        $(touch.target).trigger('swipe' + (touch.y1 - touch.y2 > 0 ? 'Up' : 'Down'));
+        touch.y1 = touch.y2 = touch.x1 = touch.x2 = touch.last = 0;
       } else if ('last' in touch) {
         touchTimeout = setTimeout(function(){
           touchTimeout = null;
@@ -33,7 +37,7 @@
     }).bind('touchcancel', function(){ touch = {} });
   });
 
-  ['swipe', 'swipeLeft', 'swipeRight', 'doubleTap', 'tap'].forEach(function(m){
+  ['swipe', 'swipeLeft', 'swipeRight', 'swipeUp', 'swipeDown', 'doubleTap', 'tap'].forEach(function(m){
     $.fn[m] = function(callback){ return this.bind(m, callback) }
   });
 })(Zepto);
