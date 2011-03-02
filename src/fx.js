6b843fddb51508a110b9da172b4606e11d5ed1f3
jaist <thejaist@gmail.com>
2011-03-02 08:10:09 -0800

    Added delay parameter, defaults at 0 if not set

--- src/fx.js
+++ src/fx.js
@@ -1,5 +1,5 @@
 (function($){
-  $.fn.anim = function(properties, duration, ease, callback){
+  $.fn.anim = function(properties, duration, ease, delay, callback){
     var transforms = [], opacity, key;
     for (key in properties)
       if (key === 'opacity') opacity = properties[key];
@@ -10,6 +10,7 @@
     return this.css({
       '-webkit-transition': 'all ' + (duration !== undefined ? duration : 0.5) + 's ' + (ease || ''),
       '-webkit-transform': transforms.join(' '),
+      '-webkit-transition-delay': (delay !== undefined ? delay : 0)+'s ',
       opacity: opacity
     });
   }
