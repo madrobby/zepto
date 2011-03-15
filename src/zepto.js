ea3d061815619923d20f6d87c3824bc6da80fbbe
Yehor Lvivski <lvivski@gmail.com>
2011-03-15 13:34:06 +0200

    Modified addClass()/removeClass() to work with multiple classnames; changed show()/hide() to work correctly with overrided default display types and inline elements

--- src/zepto.js
+++ src/zepto.js
@@ -1,11 +1,25 @@
 var Zepto = (function() {
   var slice = [].slice, key, css, $$, fragmentRE, container, document = window.document, undefined,
+    classList, elemDisplay = {},
     getComputedStyle = document.defaultView.getComputedStyle;
 
   function classRE(name){ return new RegExp("(^|\\s)" + name + "(\\s|$)") }
   function compact(array){ return array.filter(function(item){ return item !== undefined && item !== null }) }
   function flatten(array){ return [].concat.apply([], array); }
   function camelize(str){ return str.replace(/-+(.)?/g, function(match, chr){ return chr ? chr.toUpperCase() : '' }) }
+  function defaultDisplay(nodeName) {
+    if (!elemDisplay[nodeName]) {
+      var elem = document.createElement(nodeName);
+      $(document.body).append(elem);
+      var display = getComputedStyle(elem, '').getPropertyValue("display");
+      $(elem).remove();
+      if (display === "none" || display === "") {
+        display = "block"
+      }
+      elemDisplay[nodeName] = display
+    }
+    return elemDisplay[nodeName]
+  }
   function uniq(array){
     var r = [];
     for(var i=0,n=array.length;i<n;i++)
@@ -132,8 +146,27 @@ var Zepto = (function() {
       })), selector);
     },
     pluck: function(property){ return this.map(function(element){ return element[property] }) },
-    show: function(){ return this.css('display', 'block') },
-    hide: function(){ return this.css('display', 'none') },
+    show: function(){
+      return this.each(function() {
+        var styleAttr = this.getAttribute("style") || "";
+        if (styleAttr.search(/display:\s*none;?/) != -1) {
+          this.setAttribute("style", styleAttr.replace(/display:\s*none;?/, ""));
+          this.style.display = getComputedStyle(this, '').getPropertyValue("display");
+        }
+        this.style.display = this.getAttribute("data-display") || ((this.style.display != "none" && this.style.display) ? this.style.display : defaultDisplay(this.nodeName));
+        this.setAttribute("data-display", this.style.display)
+      })
+    },
+    hide: function(){
+      this.each(function(){
+        (this.style.display != "none" && this.style.display) && this.setAttribute("data-display", this.style.display)
+      });
+      return this.css("display", "none")
+    },
+    toggle: function(){
+      this.css("display") == "none" && this.show() || this.hide();
+      return this
+    },
     prev: function(){ return $(this.pluck('previousElementSibling')) },
     next: function(){ return $(this.pluck('nextElementSibling')) },
     html: function(html){
@@ -192,14 +225,24 @@ var Zepto = (function() {
       return classRE(name).test(this[0].className);
     },
     addClass: function(name){
-      return this.each(function(){
-        !$(this).hasClass(name) && (this.className += (this.className ? ' ' : '') + name)
-      });
+      return this.each(function() {
+        classList = [];
+        name.split(/\s+/g).forEach(function(klass) {
+          if (!$(this).hasClass(klass)) {
+            classList.push(klass)
+          }
+        }, this);
+        classList.length && (this.className += (this.className ? " " : "") + classList.join(" "))
+      })
     },
     removeClass: function(name){
-      return this.each(function(){
-        this.className = this.className.replace(classRE(name), ' ').trim()
-      });
+      return this.each(function() {
+        classList = this.className;
+        name.split(/\s+/g).forEach(function(klass) {
+          classList = classList.replace(classRE(klass), " ")
+        });
+        this.className = classList.trim()
+      })
     },
     toggleClass: function(name, when){
       return this.each(function(){
