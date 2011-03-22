3f264d2b043d02ca77dd485e4e86edc1fb81edfe
slavic <slavic@example.com>
2011-03-22 18:57:21 +0200

    firefox polyfill: insertAdjacentHTML(), insertAdjacentText() and insertAdjacentElement()

---
+++ src/ffpolyfill.js
@@ -0,0 +1,37 @@
+(function(undefined){
+    // insertAdjacentHTML(), insertAdjacentText() and insertAdjacentElement()
+    // for Netscape 6/Mozilla by Thor Larholm me@jscript.dk
+    if (HTMLElement !== undefined && !HTMLElement.prototype.insertAdjacentElement) {
+      HTMLElement.prototype.insertAdjacentElement = function(where,parsedNode) {
+        switch (where){
+          case 'beforeBegin':
+            this.parentNode.insertBefore(parsedNode,this);
+            break;
+          case 'afterBegin':
+            this.insertBefore(parsedNode,this.firstChild);
+            break;
+          case 'beforeEnd':
+            this.appendChild(parsedNode);
+            break;
+          case 'afterEnd':
+            if (this.nextSibling)
+              this.parentNode.insertBefore(parsedNode,this.nextSibling);
+            else 
+              this.parentNode.appendChild(parsedNode);
+            break;
+        }
+    };
+
+    HTMLElement.prototype.insertAdjacentHTML = function(where,htmlStr) {
+      var r = this.ownerDocument.createRange();
+      r.setStartBefore(this);
+      var parsedHTML = r.createContextualFragment(htmlStr);
+      this.insertAdjacentElement(where,parsedHTML);
+    };
+
+    HTMLElement.prototype.insertAdjacentText = function(where,txtStr) {
+      var parsedText = document.createTextNode(txtStr);
+      this.insertAdjacentElement(where,parsedText);
+    };
+  }
+})();
