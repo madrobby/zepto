2b6487b50198ed14d91c32ceca48888b405917d7
Thomas Fuchs <thomas@fesch.at>
2011-05-24 11:15:39 +0200

    fix misdetections of early WebOS devices, and iPads that think that they are just big iPhones, fixes #173

--- src/detect.js
+++ src/detect.js
@@ -2,9 +2,9 @@
   function detect(ua){
     var ua = ua, os = {},
       android = ua.match(/(Android)\s+([\d.]+)/),
-      iphone = ua.match(/(iPhone\sOS)\s([\d_]+)/),
       ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
-      webos = ua.match(/(webOS)\/([\d.]+)/),
+      iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
+      webos = ua.match(/(webOS)[\s\/]([\d.]+)/),
       blackberry = ua.match(/(BlackBerry).*Version\/([\d.]+)/);
     if (android) os.android = true, os.version = android[2];
     if (iphone) os.ios = true, os.version = iphone[2].replace(/_/g, '.'), os.iphone = true;
