4f5ffd521115af9a36962329023bcaf0eb292e08
Mario Estrada <mario.estrada@gmail.com>
2011-06-03 10:11:40 -0500

    Removed conflicts

--- src/fx.js
+++ src/fx.js
@@ -5,11 +5,7 @@
       if (key === 'opacity') opacity = properties[key];
       else transforms.push(key + '(' + properties[key] + ')');
 
-<<<<<<< HEAD
-    typeof callback === 'function' && this.one('webkitTransitionEnd', callback, false);
-=======
     $.isFunction(callback) && this.one('webkitTransitionEnd', callback);
->>>>>>> 79dd84c1f08e453d298c066138a9f9c07ae646ca
 
     return this.css({
       '-webkit-transition': 'all ' + (duration !== undefined ? duration : 0.5) + 's ' + (ease || ''),
