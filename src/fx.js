3be76ac66372d54064db0a5c94615900f0e52b4d
Sasha Koss <kossnocorp@gmail.com>
2011-06-17 13:11:48 +0700

    Allow to use css properties in anim function (related to issue #203)

--- src/fx.js
+++ src/fx.js
@@ -1,9 +1,27 @@
 (function($, undefined){
+
+  var supportedTransforms = [
+    'scale',     'scaleX',     'scaleY',
+    'translate', 'translateX', 'translateY', 'translate3d',
+    'skew',      'skewX',      'skewY',
+    'rotate',    'rotateX',    'rotateY',    'rotateZ',
+    'matrix'
+  ];
+
   $.fn.anim = function(properties, duration, ease, callback){
-    var transforms = [], opacity, key;
-    for (key in properties)
-      if (key === 'opacity') opacity = properties[key];
-      else transforms.push(key + '(' + properties[key] + ')');
+    var transforms = [], cssProperties = {}, key;
+
+    for (key in properties) {
+      if (supportedTransforms.indexOf(key) !== -1) {
+
+        // Transform rule
+        transforms.push(key + '(' + properties[key] + ')');
+
+      } else {
+        // CSS property
+        cssProperties[key] = properties[key];
+      }
+    }
 
     if (parseFloat(duration) !== 0) {
       $.isFunction(callback) && this.one('webkitTransitionEnd', callback);
@@ -11,10 +29,11 @@
       setTimeout(callback, 0);
     }
 
-    return this.css({
-      '-webkit-transition': 'all ' + (duration !== undefined ? duration : 0.5) + 's ' + (ease || ''),
-      '-webkit-transform': transforms.join(' '),
-      opacity: opacity
-    });
+    return this.css(
+      $.extend({
+        '-webkit-transition': 'all ' + (duration !== undefined ? duration : 0.5) + 's ' + (ease || ''),
+        '-webkit-transform': transforms.join(' ')
+      }, cssProperties)
+    );
   }
 })(Zepto);
