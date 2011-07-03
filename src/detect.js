6c877b6573d62fdb1f27a061cc76014c5d819315
Thomas Fuchs <thomas@fesch.at>
2011-07-03 13:16:12 +0200

    detect HP TouchPad

--- src/detect.js
+++ src/detect.js
@@ -5,11 +5,13 @@
       ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
       iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
       webos = ua.match(/(webOS|hpwOS)[\s\/]([\d.]+)/),
+      touchpad = webos && ua.match(/TouchPad/),
       blackberry = ua.match(/(BlackBerry).*Version\/([\d.]+)/);
     if (android) os.android = true, os.version = android[2];
     if (iphone) os.ios = true, os.version = iphone[2].replace(/_/g, '.'), os.iphone = true;
     if (ipad) os.ios = true, os.version = ipad[2].replace(/_/g, '.'), os.ipad = true;
     if (webos) os.webos = true, os.version = webos[2];
+    if (touchpad) os.touchpad = true;
     if (blackberry) os.blackberry = true, os.version = blackberry[2];
     return os;
   }
