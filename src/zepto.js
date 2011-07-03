411dc7346d9ab9e8bc9964fd50432c7e50d4de54
Edward Tsech <edtsech@gmail.com>
2011-07-03 19:37:55 +0700

    Define css variable locally

--- src/zepto.js
+++ src/zepto.js
@@ -1,5 +1,5 @@
 var Zepto = (function() {
-  var undefined, key, css, $$, classList,
+  var undefined, key, $$, classList,
     emptyArray = [], slice = emptyArray.slice,
     document = window.document,
     elementDisplay = {}, classCache = {},
@@ -277,7 +277,7 @@ var Zepto = (function() {
     css: function(property, value){
       if (value === undefined && typeof property == 'string')
         return this[0].style[camelize(property)] || getComputedStyle(this[0], '').getPropertyValue(property);
-      css = '';
+      var css = '';
       for (key in property) css += dasherize(key) + ':' + maybeAddPx(key, property[key]) + ';';
       if (typeof property == 'string') css = dasherize(property) + ":" + maybeAddPx(property, value);
       return this.each(function() { this.style.cssText += ';' + css });
