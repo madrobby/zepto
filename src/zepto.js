ebef7562925dd7c949ca970aeea26035a68be9f5
Paul Miller <paulmillr@me.com>
2011-07-05 23:17:06 +0200

    fix wrap() for Firefox

--- src/zepto.js
+++ src/zepto.js
@@ -212,7 +212,7 @@ var Zepto = (function() {
     },
     wrap: function(newContent) {
       return this.each(function() {
-        $(this).wrapAll($(newContent)[0].cloneNode());
+        $(this).wrapAll($(newContent)[0].cloneNode(false));
       });
     },
     wrapAll: function(newContent) {
