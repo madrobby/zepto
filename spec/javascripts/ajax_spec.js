b3f6cfed1de99410d917145ee89384c99880c671
Sasha Koss <kossnocorp@gmail.com>
2011-08-11 11:42:16 +0700

    Write example spec for ajax.js

---
+++ spec/javascripts/ajax_spec.js
@@ -0,0 +1,7 @@
+describe('Ajax', function () {
+    describe('$.ajax', function () {
+        it('should be defined', function () {
+            expect($.ajax).toBeDefined();
+        });
+    });
+});
