04af6bc5629e309e9ae0e32f5199b29ad974c8be
Sasha Koss <kossnocorp@gmail.com>
2011-08-12 08:50:53 +0700

    Basic examples for all ajax functions

--- spec/javascripts/ajax_spec.js
+++ spec/javascripts/ajax_spec.js
@@ -3,5 +3,51 @@ describe('Ajax', function () {
         it('should be defined', function () {
             expect($.ajax).toBeDefined();
         });
+
+        it('should returns xhr object', function () {
+            expect($.ajax().constructor).toBe(XMLHttpRequest);
+        });
+    });
+
+    describe('$.get', function () {
+        it('should be defined', function () {
+            expect($.get).toBeDefined();
+        });
+    });
+
+    describe('$.post', function () {
+        it('should be defined', function () {
+            expect($.post).toBeDefined();
+        });
+    });
+
+    describe('$.getJSON', function () {
+        it('should be defined', function () {
+            expect($.post).toBeDefined();
+        });
+    });
+
+    describe('$.load', function () {
+        it('should be defined', function () {
+            expect($.post).toBeDefined();
+        });
+    });
+
+    describe('$.param', function () {
+        it('should be defined', function () {
+            expect($.post).toBeDefined();
+        });
+    });
+
+    describe('$.ajaxSettings', function () {
+        it('should be defined', function () {
+            expect($.ajaxSettings).toBeDefined();
+        });
+    });
+
+    describe('$.ajaxJSONP', function () {
+        it('should be defined', function () {
+            expect($.ajaxJSONP).toBeDefined();
+        });
     });
 });
