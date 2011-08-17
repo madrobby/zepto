0362528aa7a17c3742921b25bac85417ec851c24
Sasha Koss <kossnocorp@gmail.com>
2011-08-17 22:57:40 +0700

    Fix issue #258

--- src/zepto.js
+++ src/zepto.js
@@ -326,8 +326,13 @@ var Zepto = (function() {
       };
     },
     css: function(property, value){
-      if (value === undefined && typeof property == 'string')
-        return this[0].style[camelize(property)] || getComputedStyle(this[0], '').getPropertyValue(property);
+      if (value === undefined && typeof property == 'string') {
+        return(
+          this.length == 0
+            ? undefined
+            : this[0].style[camelize(property)] || getComputedStyle(this[0], '').getPropertyValue(property)
+        );
+      }
       var css = '';
       for (key in property) css += dasherize(key) + ':' + maybeAddPx(key, property[key]) + ';';
       if (typeof property == 'string') css = dasherize(property) + ":" + maybeAddPx(property, value);
