ca859209f29be22a784d23be478cea810ff995e2
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-09-02 10:36:49 +0200

    refactor attr() to support empty attributes and more input field types
    Changes:
     - for empty attributes (e.g. `<div data-empty>`) it now returns an empty string
     - for non-existent attributes it now returns null instead of undefined
     - attr('value') now uses the `value` property directly for all input
       types, not just `<input type="text">`

--- src/zepto.js
+++ src/zepto.js
@@ -294,9 +294,12 @@ var Zepto = (function() {
         this.each(function(){ this.textContent = text });
     },
     attr: function(name, value){
+      var res;
       return (typeof name == 'string' && value === undefined) ?
-        (this.length > 0 && this[0].nodeName == 'INPUT' && this[0].type == 'text' && name == 'value') ? (this.val()) :
-        (this.length > 0 ? this[0].getAttribute(name) || (name in this[0] ? this[0][name] : undefined) : undefined) :
+        (this.length == 0 ? undefined :
+          (name == 'value' && this[0].nodeName == 'INPUT') ? this.val() :
+          (!(res = this[0].getAttribute(name)) && name in this[0]) ? this[0][name] : res
+        ) :
         this.each(function(idx){
           if (isO(name)) for (key in name) this.setAttribute(key, name[key])
           else this.setAttribute(name, funcArg(this, value, idx, this.getAttribute(name)));
