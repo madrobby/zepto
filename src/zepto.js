3b280fdefdf08ea8570e4bd4ef4f37f4a61066f5
Ross Hale <ross@socialstay.com>
2011-09-21 01:41:01 -0700

    replaceWith, wrap, before, after: make all of them noop on fragments
    Nodes in a fragment have no parent so these operations aren't possible.
    Making them a noop ensures they don't error out.

--- src/zepto.js
+++ src/zepto.js
@@ -419,7 +419,7 @@ var Zepto = (function() {
 
   function insert(operator, target, node) {
     var parent = (operator % 2) ? target : target.parentNode;
-    parent.insertBefore(node,
+    parent && parent.insertBefore(node,
       !operator ? target.nextSibling :      // after
       operator == 1 ? parent.firstChild :   // prepend
       operator == 2 ? target :              // before
