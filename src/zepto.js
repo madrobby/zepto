c711f8d6e5ec2d6d6e8dd9a72a3a8575f0c9546e
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-11-21 11:43:16 +0200

    css(property,'') removes the style as in jquery - both usage cases

--- src/zepto.js
+++ src/zepto.js
@@ -439,10 +439,20 @@ var Zepto = (function() {
             : this[0].style[camelize(property)] || getComputedStyle(this[0], '').getPropertyValue(property)
         )
       }
-      var css = ''
-      for (key in property) css += dasherize(key) + ':' + maybeAddPx(key, property[key]) + ';'
-      if (typeof property == 'string') css = dasherize(property) + ":" + maybeAddPx(property, value)
-      return this.each(function(){ this.style.cssText += ';' + css })
+      var css = '';
+      for (key in property) {
+        if(typeof property[key] == 'string' && property[key] == '')
+          this.each(function() {this.style.removeProperty(dasherize(key))});
+        else
+          css += dasherize(key) + ':' + maybeAddPx(key, property[key]) + ';';
+      }
+      if (typeof property == 'string') {
+        if (value == '')
+          this.each(function() {this.style.removeProperty(dasherize(property))});
+        else
+          css = dasherize(property) + ":" + maybeAddPx(property, value);
+      }
+      return this.each(function() { this.style.cssText += ';' + css });
     },
     index: function(element){
       return element ? this.indexOf($(element)[0]) : this.parent().children().indexOf(this[0])
