e796f3040de62c07433326ff4da4a3ceb966c32e
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-11-26 21:22:47 +0100

    fix $.each context
    Closes #295

--- src/zepto.js
+++ src/zepto.js
@@ -143,11 +143,11 @@ var Zepto = (function() {
     var i, key;
     if (likeArray(elements))
       for(i = 0; i < elements.length; i++) {
-        if(callback(i, elements[i]) === false) return elements;
+        if(callback.call(elements[i], i, elements[i]) === false) return elements;
       }
     else
       for(key in elements) {
-        if(callback(key, elements[key]) === false) return elements;
+        if(callback.call(elements[key], key, elements[key]) === false) return elements;
       }
     return elements;
   }
