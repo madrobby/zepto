5179f9b6de70ba2c6d65d40bd5a945cb3e3ebb91
Drew Stokes <dstokes@beachmint.com>
2012-02-09 15:26:04 -0800

    return empty array when querying invalid node type

--- src/zepto.js
+++ src/zepto.js
@@ -105,6 +105,7 @@ var Zepto = (function() {
     var found;
     return (element === document && idSelectorRE.test(selector)) ?
       ( (found = element.getElementById(RegExp.$1)) ? [found] : emptyArray ) :
+      (element.nodeType !== 1 && element.nodeType !== 9) ? emptyArray :
       slice.call(
         classSelectorRE.test(selector) ? element.getElementsByClassName(RegExp.$1) :
         tagSelectorRE.test(selector) ? element.getElementsByTagName(selector) :
