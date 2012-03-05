6f191d46bcefd00549593cc118b56814a5ca6184
Leonardo Dutra <leodutra.br@gmail.com>
2012-03-05 20:52:53 -0300

    Replaced "undefined" by "void 0". Added comment.

--- src/polyfill.js
+++ src/polyfill.js
@@ -2,24 +2,26 @@
 //     (c) 2010-2012 Thomas Fuchs
 //     Zepto.js may be freely distributed under the MIT license.
 
-(function(undefined){
-  if (String.prototype.trim === undefined) // fix for iOS 3.2
+(function(){
+  if (String.prototype.trim === void 0) // fix for iOS 3.2
     String.prototype.trim = function(){ this.replace(/^\s+|\s+$/g, '') }; // smaller trim (from jQuery)
     
   // For iOS 3.x
   // from https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/reduce
   if (!Array.prototype.reduce) {
     Array.prototype.reduce = function reduce(accumulator){
-      var i = 0, obj = this, l = obj.length >> 0, curr; // ">> 0" avoids null test later
+      var i = 0, obj = this, l = obj.length >> 0, curr;
 
-      /* null and undefined never got length. That's why we don't need "Object(this)". 
-       * All the other possibilities are objects in js
+      /* null and undefined will always be replaced by the global (window) on ".call"s. 
+       * Any other input (Number, Boolean, ..) will have no length, generating a TypeError.
+       * That's why we don't need "Object(this)".
+       * The issue is: how could we receive and avoid the real null or undefined values on reduce.calls?
        */
 
-      if(typeof accumulator !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+      if (typeof accumulator !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
         throw new TypeError();
 
-      if(arguments.length < 2) { // "<= 1" is slower
+      if (arguments.length < 2) { // "<= 1" is slower
         if (l === 0) throw new TypeError(); // just inside the only test for "arguments <= 1"
         curr = obj[0]; // Increase i to start searching the secondly defined element in the array
         i = 1; // start accumulating at the second element
@@ -27,7 +29,7 @@
       else curr = arguments[1];
 
       for (;i < l;++i) // "for" is smaller than "while"; ++i is faster than i++
-        if(i in obj) curr = accumulator.call(undefined, curr, obj[i], i, obj);
+        if (i in obj) curr = accumulator.call({}, curr, obj[i], i, obj);
 
       return curr;
     };
