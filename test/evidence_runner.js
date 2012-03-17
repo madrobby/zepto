8f1834f814dacde1df2e413c65d41524589bf7ea
Thomas Fuchs <thomas@fesch.at>
2012-03-17 17:50:01 +0100

    show number of assertions when running unit tests

--- test/evidence_runner.js
+++ test/evidence_runner.js
@@ -156,11 +156,11 @@
     var container = $('results')
     if (container) {
       if (results.failureCount || results.errorCount) {
-        container.innerHTML = printf("Finished in %d s. &ndash; <em>%d failures, %d errors</em>",
-          [seconds, results.failureCount, results.errorCount])
+        container.innerHTML = printf("Finished in %d s. &ndash; <em>%d failures, %d errors</em> (%d assertions)",
+          [seconds, results.failureCount, results.errorCount, results.assertionCount])
         container.className = 'failed'
       } else {
-        container.innerHTML = printf("Finished in %d s. &ndash; <em>%d tests passed</em>", [seconds, results.testCount])
+        container.innerHTML = printf("Finished in %d s. &ndash; <em>%d tests passed</em> (%d assertions)", [seconds, results.testCount, results.assertionCount])
         container.className = 'passed'
       }
     }
