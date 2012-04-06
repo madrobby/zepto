6bcba9545f04768fd20e35c3f255f8d7f00f02e1
Rémi <remi.chaignon@gmail.com>
2012-04-05 18:17:35 -0600

    Switch to single quote

--- src/data.js
+++ src/data.js
@@ -23,7 +23,7 @@
       var i, l, attributes = node.attributes;
       for (i = 0, l = attributes.length; i < l; i++) {
         name = attributes[i].name
-        if ( name.indexOf( "data-" ) === 0 ) store[name.substring(5)] = attributes[i].value
+        if ( name.indexOf('data-') === 0 ) store[name.substring(5)] = attributes[i].value
       }
     }
     return store
