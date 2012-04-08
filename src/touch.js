f3152641a6fcaf030f39762a02056ca70ae1f49e
Thomas Fuchs <thomas@fesch.at>
2012-04-08 19:29:27 +0200

    add automated unit tests for touch module; missing longtap and specific swipe directions

--- src/touch.js
+++ src/touch.js
@@ -46,7 +46,7 @@
         touch.el.trigger('doubleTap')
         touch = {}
       } else if ((touch.x2 && Math.abs(touch.x1 - touch.x2) > 30) ||
-                 (touch.y2 && Math.abs(touch.y1 - touch.y2) > 30)) {                 
+                 (touch.y2 && Math.abs(touch.y1 - touch.y2) > 30)) {
         touch.el.trigger('swipe') &&
           touch.el.trigger('swipe' + (swipeDirection(touch.x1, touch.x2, touch.y1, touch.y2)))
         touch.x1 = touch.x2 = touch.y1 = touch.y2 = touch.last = 0
