62c8c7eecef765e901c83f16f9a160d13310137c
Lee Mallabone <lee@rapportive.com>
2012-05-08 12:42:11 -0700

    Don't evaluate <script> tag bodies when src is present

--- src/zepto.js
+++ src/zepto.js
@@ -562,7 +562,8 @@ var Zepto = (function() {
         for (var i = 0; i < nodes.length; i++) {
           var node = nodes[inReverse ? nodes.length-i-1 : i]
           traverseNode(node, function(node){
-            if (node.nodeName != null && node.nodeName.toUpperCase() === 'SCRIPT' && (!node.type || node.type === 'text/javascript'))
+            if (node.nodeName != null && node.nodeName.toUpperCase() === 'SCRIPT' &&
+               (!node.type || node.type === 'text/javascript') && !node.src)
               window['eval'].call(window, node.innerHTML)
           })
           if (copyByClone && index < size - 1) node = node.cloneNode(true)
