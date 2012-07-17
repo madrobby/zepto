a107ace2c3d61fb0303308572f7e5d3c568a3c77
Yuta Yamashita <yuta-yamashita@gree.co.jp>
2012-07-17 17:41:33 +0900

    Change the order of evaluating the script content
    In jQuery, all the nodes appeared before a script tag are inserted when
    the script content is evaluated, but in zepto, all nodes before the
    script is inserted *after* the script is evaluated.
    Changed it to behave the same with jQuery.

--- src/zepto.js
+++ src/zepto.js
@@ -571,13 +571,13 @@ var Zepto = (function() {
 
       return this.each(function(index, target){
         for (var i = 0; i < nodes.length; i++) {
+          if (copyByClone && index < size - 1) node = node.cloneNode(true)
+          insert(operator, target, node)
           var node = nodes[inReverse ? nodes.length-i-1 : i]
           traverseNode(node, function(node){
             if (node.nodeName != null && node.nodeName.toUpperCase() === 'SCRIPT' && (!node.type || node.type === 'text/javascript'))
               window['eval'].call(window, node.innerHTML)
           })
-          if (copyByClone && index < size - 1) node = node.cloneNode(true)
-          insert(operator, target, node)
         }
       })
     }
