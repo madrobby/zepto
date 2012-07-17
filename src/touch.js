0d9b3b9e6242c848705899714064814c52320658
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-07-17 17:48:55 +0200

    fix whitespace

--- src/touch.js
+++ src/touch.js
@@ -64,7 +64,7 @@
         // swipe
         if ((touch.x2 && Math.abs(touch.x1 - touch.x2) > 30) ||
             (touch.y2 && Math.abs(touch.y1 - touch.y2) > 30))
-          
+
           swipeTimeout = setTimeout(function() {
             touch.el.trigger('swipe')
             touch.el.trigger('swipe' + (swipeDirection(touch.x1, touch.x2, touch.y1, touch.y2)))
