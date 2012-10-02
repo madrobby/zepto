e32c3ce4293094d4a7df5d5cfd8fb5e30316f5e3
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-10-02 19:30:17 +0200

    ensure event created in $.Event() has isDefaultPrevented()
    This is a dirty hack right now because it still doesn't mean all native
    events support this method, but this goes a long way of supporting
    plugins.

--- src/event.js
+++ src/event.js
@@ -223,6 +223,7 @@
     var event = document.createEvent(specialEvents[type] || 'Events'), bubbles = true
     if (props) for (var name in props) (name == 'bubbles') ? (bubbles = !!props[name]) : (event[name] = props[name])
     event.initEvent(type, bubbles, true, null, null, null, null, null, null, null, null, null, null, null, null)
+    event.isDefaultPrevented = function(){ return this.defaultPrevented }
     return event
   }
 
