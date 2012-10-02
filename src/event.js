4a2f5e4404296a19755aa2abedac110b1f10bcc6
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-10-02 21:41:48 +0200

    `return false` in event handler calls stopPropagation()

--- src/event.js
+++ src/event.js
@@ -45,9 +45,9 @@
     eachEvent(events, fn, function(event, fn){
       var delegate = getDelegate && getDelegate(fn, event),
         callback = delegate || fn
-      var proxyfn = function (event) {
-        var result = callback.apply(element, [event].concat(event.data))
-        if (result === false) event.preventDefault()
+      var proxyfn = function (e) {
+        var result = callback.apply(element, [e].concat(e.data))
+        if (result === false) e.preventDefault(), e.stopPropagation()
         return result
       }
       var handler = $.extend(parse(event), {fn: fn, proxy: proxyfn, sel: selector, del: delegate, i: set.length})
