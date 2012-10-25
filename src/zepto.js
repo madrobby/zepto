f0cd5f688193248d8c8ca6bacc063b34d98d9dce
shalecraig <shalecraig@gmail.com>
2012-10-25 02:24:47 -0700

    Removed another ternary operator inside src/zepto.js.
    Uglify.js isn't very smart, and doesn't do variable inlining like
    this may take advantage of.
    
    That being said, I think it's worth the extra 10-15 bytes for more
    readability and the ability to blame without fearing "death"

--- src/zepto.js
+++ src/zepto.js
@@ -412,10 +412,16 @@ var Zepto = (function() {
           if (!selector.call(this,idx)) nodes.push(this)
         })
       else {
-        var excludes = typeof selector == 'string' ? this.filter(selector) :
-          (likeArray(selector) && isFunction(selector.item)) ? slice.call(selector) : $(selector)
+        var excludes;
+        if (typeof selector == 'string')
+          excludes = this.filter(selector)
+        else if (likeArray(selector) && isFunction(selector.item))
+          excludes =  slice.call(selector)
+        else 
+          excludes = $(selector)
         this.forEach(function(el){
-          if (excludes.indexOf(el) < 0) nodes.push(el)
+          if (excludes.indexOf(el) < 0)
+            nodes.push(el)
         })
       }
       return $(nodes)
