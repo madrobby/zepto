38dddbdf455ed9b4d7517f39496416cdecd3a05d
shalecraig <shalecraig@gmail.com>
2012-10-25 02:18:13 -0700

    Another change that doesn't impact the min.js file.

--- src/touch.js
+++ src/touch.js
@@ -15,7 +15,13 @@
 
   function swipeDirection(x1, x2, y1, y2) {
     var xDelta = Math.abs(x1 - x2), yDelta = Math.abs(y1 - y2)
-    return xDelta >= yDelta ? (x1 - x2 > 0 ? 'Left' : 'Right') : (y1 - y2 > 0 ? 'Up' : 'Down')
+    if (xDelta >= yDelta)
+      if (x1 - x2) > 0
+        return 'Left'
+      return 'Right'
+    if (y1 - y2) > 0
+      return 'Up'
+    return 'Down'
   }
 
   function longTap() {
