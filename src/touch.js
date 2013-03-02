d945672b89be7957b268e01df1dc6425de5932b1
Thomas Fuchs <thomas@fesch.at>
2013-03-02 17:20:15 -0500

    Prevent scrolling when swipe up/down is detected

--- src/touch.js
+++ src/touch.js
@@ -57,6 +57,8 @@
         cancelLongTap()
         touch.x2 = e.touches[0].pageX
         touch.y2 = e.touches[0].pageY
+        if (Math.abs(touch.x1 - touch.x2) > 10)
+          e.preventDefault()
       })
       .bind('touchend', function(e){
          cancelLongTap()
