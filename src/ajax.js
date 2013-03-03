9b77aca0349903aecc820c028580428896df6832
Mislav Marohnić <mislav.marohnic@gmail.com>
2013-03-02 21:15:00 -0500

    Ajax: fix parsing response Content-Type from the server
    Fixes #699

--- src/ajax.js
+++ src/ajax.js
@@ -148,6 +148,7 @@
   }
 
   function mimeToDataType(mime) {
+    if (mime) mime = mime.split(';', 2)[0]
     return mime && ( mime == htmlType ? 'html' :
       mime == jsonType ? 'json' :
       scriptTypeRE.test(mime) ? 'script' :
