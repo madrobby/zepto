5269256d40a8faeabff29c67dea1ab26d289e20a
Stefan Andres Charsley <charsleysa@gmail.com>
2013-05-01 02:47:25 +1200

    Stopped __proto__ from being enumerable
    This stops __proto__ from being enumerable which means that it won't
    appear in for in loops as it did previously. Here's an example of what
    the old version does and how to fix it.
    http://jsfiddle.net/dyTJ6/

--- src/ie.js
+++ src/ie.js
@@ -1,10 +1,11 @@
 // IE Support
-// This module provides a very basic IE Support fix by adding the __proto__ property to objects.
+// This module provides a IE Support to all __proto__ dependant libraries by adding the __proto__ property to objects.
 ;(function(undefined){
     if (!('__proto__' in {})) {
         Object.defineProperty(Object.prototype, '__proto__', {
             set: function (x) {
                 for (var prop in x) {
+                    // Stops stack overflow errors
                     if (prop == '__proto__') continue;
                     this[prop] = x[prop];
                 }
@@ -12,7 +13,7 @@
             get: function () {
                 return this;
             },
-            enumerable: true,
+            enumerable: false,
             configurable: true
         });
     }
