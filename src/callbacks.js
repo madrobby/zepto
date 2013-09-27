ef55122da7e07b67246dbc8b0ffacb37d1b59571
Caitlin Potter <snowball@defpixel.com>
2013-09-27 09:00:17 -0400

    Address codereview from @mislav
    I'll rebase this later. I'm a bit iffy about how to write some of this prose,
    some of it seems to have some very subtle semantic meaning which I worry about
    breaking.

--- src/callbacks.js
+++ src/callbacks.js
@@ -3,18 +3,12 @@
 //     Zepto.js may be freely distributed under the MIT license.
 
 ;(function($){
-  // Simplified implementation of jQuery's $.Callbacks helper, relied upon by $.ajax and
-  // $.Deferred.
-  //
-  // Unlike jQuery, this implementation supports passing of parameters only as objects,
-  // which may be slightly inconvenient, but also should performs slightly better than
-  // jQuery's implementation involving string manipulation.
-  //
+  // Create a collection of callbacks to be fired in a sequence, with configurable behaviour
   // Option flags:
-  //   - once
-  //   - memory
-  //   - stopOnFalse
-  //   - unique
+  //   - once: Callbacks fired at most one time.
+  //   - memory: Remember the most recent context and arguments
+  //   - stopOnFalse: Cease iterating over callback list
+  //   - unique: Permit adding at most one instance of the same callback
   $.Callbacks = function(options) {
     if (options && typeof options !== 'object') {
       return;
@@ -38,57 +32,39 @@
           firing = true
           for ( ; list && firingIndex < firingLength ; ++firingIndex ) {
             if (list[firingIndex].apply(data[0], data[1]) === false && options.stopOnFalse) {
-              memory = false // To prevent further calls using add
+              memory = false
               break
             }
           }
           firing = false
           if (list) {
-            if (stack) {
-              if (stack.length) {
-                fire(stack.shift())
-              }
-            } else if (memory) {
-              list.length = 0
-            } else {
-              Callbacks.disable()
-            }
+            if (stack) stack.length && fire(stack.shift())
+            else if (memory) list.length = 0
+            else Callbacks.disable()
           }
         },
-        
-        // Callbacks list object
+
         Callbacks = {
-          // Add a callback or a collection of callbacks to the list
           add: function() {
             if (list) {
-              // First, we save the current length
               var start = list.length
               function _add(args) {
                 $.each(args, function(_, arg) {
                   if (typeof arg === "function") {
-                    if (!options.unique || !Callbacks.has(arg)) {
-                      list.push(arg)
-                    }
-                  } else if (arg && arg.length && typeof arg !== "string") { // $.isArray?
-                    _add(arg)
-                  }
+                    if (!options.unique || !Callbacks.has(arg)) list.push(arg)
+                  } 
+                  else if (arg && arg.length && typeof arg !== 'string') _add(arg)
                 })
               }
               _add(arguments)
-              // Do we need to add the callbacks to the
-              // current firing batch?
-              if (firing) {
-                firingLength = list.length
-              // With memory, if we're not firing then
-              // we should call right away
-              } else if (memory) {
+              if (firing) firingLength = list.length
+              else if (memory) {
                 firingStart = start
                 fire(memory)
               }
             }
             return this
           },
-          // Remove a callback from the list
           remove: function() {
             if (list) {
               $.each(arguments, function(_, arg) {
@@ -97,38 +73,28 @@
                   list.splice(index, 1)
                   // Handle firing indexes
                   if (firing) {
-                    if (index <= firingLength) {
-                      --firingLength
-                    }
-                    if (index <= firingIndex) {
-                      --firingIndex
-                    }
+                    if (index <= firingLength) --firingLength
+                    if (index <= firingIndex) --firingIndex
                   }
                 }
               })
             }
             return this
           },
-          // Check if a given callback is in the list.
-          // If no argument is given, return whether or not list has callbacks attached.
           has: function(fn) {
             return !!(list && (fn ? $.inArray(fn, list) > -1 : list.length))
           },
-          // Remove all callbacks from the list
           empty: function() {
             firingLength = list.length = 0
             return this
           },
-          // Have the list do nothing anymore
           disable: function() {
             list = stack = memory = undefined
             return this
           },
-          // Is it disabled?
           disabled: function() {
             return !list
           },
-          // Lock the list in its current state
           lock: function() {
             stack = undefined;
             if (!memory) {
@@ -136,28 +102,21 @@
             }
             return this
           },
-          // Is it locked?
           locked: function() {
             return !stack
           },
-          // Call all callbacks with the given context and arguments
           fireWith: function(context, args) {
             if (list && (!fired || stack)) {
               args = args || []
               args = [context, args.slice ? args.slice() : args]
-              if (firing) {
-                stack.push(args)
-              } else {
-                fire(args)
-              }
+              if (firing) stack.push(args)
+              else fire(args)
             }
             return this
           },
-          // Call all the callbacks with the given arguments
           fire: function() {
             return Callbacks.fireWith(this, arguments)
           },
-          // To know if the callbacks have already been called at least once
           fired: function() {
             return !!fired
           }
--- src/deferred.js
+++ src/deferred.js
@@ -24,7 +24,6 @@
             return $.Deferred(function(defer) {
               $.each(tuples, function(i, tuple) {
                 var fn = $.isFunction(fns[i]) && fns[i]
-                // deferred[ done | fail | progress ] for forwarding actions to newDefer
                 deferred[tuple[1]](function() {
                   var returned = fn && fn.apply(this, arguments)
                   if (returned && $.isFunction(returned.promise)) {
@@ -33,7 +32,9 @@
                       .fail(defer.reject)
                       .progress(defer.notify)
                   } else {
-                    defer[tuple[0] + "With"](this === promise ? defer.promise() : this, fn ? [returned] : arguments)
+                    var context = this === promise ? defer.promise() : this,
+                        values = fn ? [returned] : arguments
+                    defer[tuple[0] + "With"](context, values)
                   }
                 })
               })
@@ -50,41 +51,31 @@
     // Backwards compatibility alias -- Omit?
     promise.pipe = promise.then
 
-    // List-specific methods
     $.each(tuples, function(i, tuple) {
       var list = tuple[2],
           stateString = tuple[3]
 
-      // promise[ done | fail | progress ] = list.add
       promise[tuple[1]] = list.add
 
-      // Handle state
       if (stateString) {
         list.add(function() {
-          // state = [ resolved | rejected ]
           state = stateString
-
-          // [ reject_list | resolve_list ].disable progress_list.lock
         }, tuples[i^1][2].disable, tuples[2][2].lock)
       }
 
-      // deferred[ resolve | reject | notify ]
       deferred[tuple[0]] = function() {
         deferred[tuple[0] + "With"](this === deferred ? promise : this, arguments)
         return this
       }
       deferred[tuple[0] + "With"] = list.fireWith
     })
-    
-    // Make the deferred a promise
+
     promise.promise(deferred)
 
-    // Call given func if any
     if (func) {
       func.call(deferred, deferred)
     }
 
-    // Return created object
     return deferred
   }
   $.when = function(sub) {
