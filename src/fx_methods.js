acbc9d700e5892b1033589487a6882b0a294df6f
Acemir Sousa Mendes <acemirsm@gmail.com>
2013-10-17 03:35:40 -0300

    Fix for #445
    Fix for #445
    Commente here: https://github.com/madrobby/zepto/issues/445#issuecomment-26481175

--- src/fx_methods.js
+++ src/fx_methods.js
@@ -25,14 +25,11 @@
 
   $.fn.show = function(speed, callback) {
     origShow.call(this)
-    if (speed === undefined) speed = 0
-    else this.css('opacity', 0)
-    return anim(this, speed, 1, '1,1', callback)
+    return (speed === undefined) ? 0 : anim(this, speed, 1, '1,1', callback)
   }
-
+  
   $.fn.hide = function(speed, callback) {
-    if (speed === undefined) return origHide.call(this)
-    else return hide(this, speed, '0,0', callback)
+    return (speed === undefined) ? origHide.call(this) : hide(this, speed, '0,0', callback)
   }
 
   $.fn.toggle = function(speed, callback) {
