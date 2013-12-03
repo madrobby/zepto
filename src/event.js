df77bfba9cd708b7acfda48257af3368ac2f4982
Mislav Marohnić <mislav.marohnic@gmail.com>
2013-12-03 23:28:59 +0100

    Don't let the event delegation selector match the host element
    jQuery compatibility
    
    Fixes #801

--- src/event.js
+++ src/event.js
@@ -185,7 +185,7 @@
 
       if (selector) delegator = function(e){
         var evt, match = $(e.target).closest(selector, element).get(0)
-        if (match) {
+        if (match && match !== element) {
           evt = $.extend(createProxy(e), {currentTarget: match, liveFired: element})
           return (autoRemove || callback).apply(match, [evt].concat(slice.call(arguments, 1)))
         }
