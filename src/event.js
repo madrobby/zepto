madrobby/zepto

commit a62a32b04356a5231bb5a4e13cb0a767e1824ca6

Fix delegating focus/blur events in IE
These events don't bubble natively in any browser, so we used event
capturing to provide event delegation support for them. This workaround
doesn't work in IE, however.

This translates focus/blur events to focusin/focusout, which bubble
natively.

A potential pitfall of this change is if a user inspects `event.type`
inside an event handler for "focus", for instance, they will get
"focusin" back.

References #869, fixes #868

src/event.js
@@ -9,6 +9,7 @@
       isString = function(obj){ return typeof obj == 'string' },
       handlers = {},
       specialEvents={},
+      focus = { focus: 'focusin', blur: 'focusout' },
       hover = { mouseenter: 'mouseover', mouseleave: 'mouseout' }
 
   specialEvents.click = specialEvents.mousedown = specialEvents.mouseup = specialEvents.mousemove = 'MouseEvents'
@@ -37,12 +38,11 @@
 
   function eventCapture(handler, captureSetting) {
     return handler.del &&
-      (handler.e == 'focus' || handler.e == 'blur') ||
       !!captureSetting
   }
 
   function realEvent(type) {
-    return hover[type] || type
+    return hover[type] || focus[type] || type
   }
 
   function add(element, events, fn, data, selector, delegator, capture){

commit 0bf68f89edd0ce0eeda719d9c19ebf0caf8f6577

Fix focus/blur delegation in Firefox and Android
This was broken in a62a32b04356a5231bb5a4e13cb0a767e1824ca6 that tried
to fix the same in IE.

src/event.js
@@ -9,6 +9,7 @@
       isString = function(obj){ return typeof obj == 'string' },
       handlers = {},
       specialEvents={},
+      focusinSupported = 'onfocusin' in window,
       focus = { focus: 'focusin', blur: 'focusout' },
       hover = { mouseenter: 'mouseover', mouseleave: 'mouseout' }
 
@@ -38,11 +39,12 @@
 
   function eventCapture(handler, captureSetting) {
     return handler.del &&
+      (!focusinSupported && (handler.e in focus)) ||
       !!captureSetting
   }
 
   function realEvent(type) {
-    return hover[type] || focus[type] || type
+    return hover[type] || (focusinSupported && focus[type]) || type
   }
 
   function add(element, events, fn, data, selector, delegator, capture){