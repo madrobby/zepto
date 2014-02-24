d240695ba0e0b0cf0bec2af848553eb42d6bb949
David Morrow <dperrymorrow@gmail.com>
2014-02-24 11:25:33 -0800

    Adding isNumeric to Zepto #847

--- src/zepto.js
+++ src/zepto.js
@@ -66,7 +66,7 @@ var Zepto = (function() {
     return obj == null ? String(obj) :
       class2type[toString.call(obj)] || "object"
   }
-
+  function isNumeric(value)  { return type(parseFloat(value)) == "number" }
   function isFunction(value) { return type(value) == "function" }
   function isWindow(obj)     { return obj != null && obj == obj.window }
   function isDocument(obj)   { return obj != null && obj.nodeType == obj.DOCUMENT_NODE }
@@ -315,6 +315,7 @@ var Zepto = (function() {
 
   $.type = type
   $.isFunction = isFunction
+  $.isNumeric = isNumeric
   $.isWindow = isWindow
   $.isArray = isArray
   $.isPlainObject = isPlainObject
