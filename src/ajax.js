0d4b036a8099a405cf19623dd3183572c78e241d
Mohammed Le Doze <mledoze@users.noreply.github.com>
2015-03-19 10:07:16 +0100

    add comment about `urlAnchor.href` self assignment
    This comment is added to avoid future question about `urlAnchor.href` self assignment, see #1085.

--- src/ajax.js
+++ src/ajax.js
@@ -196,6 +196,7 @@
     if (!settings.crossDomain) {
       urlAnchor = document.createElement('a')
       urlAnchor.href = settings.url
+      // cleans up URL for .href (IE only), see https://github.com/madrobby/zepto/pull/1049
       urlAnchor.href = urlAnchor.href
       settings.crossDomain = (originAnchor.protocol + '//' + originAnchor.host) !== (urlAnchor.protocol + '//' + urlAnchor.host)
     }
