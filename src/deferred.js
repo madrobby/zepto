601372ac4e3f98d502c707bf841589fbc48a3a7d
Thomas Fuchs <thomas@fesch.at>
2015-12-31 08:53:03 -0500

    🎉 Happy new year!

--- src/deferred.js
+++ src/deferred.js
@@ -1,5 +1,5 @@
 //     Zepto.js
-//     (c) 2010-2015 Thomas Fuchs
+//     (c) 2010-2016 Thomas Fuchs
 //     Zepto.js may be freely distributed under the MIT license.
 //
 //     Some code (c) 2005, 2013 jQuery Foundation, Inc. and other contributors
--- src/ios3.js
+++ src/ios3.js
@@ -1,5 +1,5 @@
 //     Zepto.js
-//     (c) 2010-2015 Thomas Fuchs
+//     (c) 2010-2016 Thomas Fuchs
 //     Zepto.js may be freely distributed under the MIT license.
 
 ;(function(undefined){
