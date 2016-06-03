49d2f8de4fa5e09a47e8de13eece548f76e0f6e1
sunnylost <sunnylost@gmail.com>
2016-06-03 11:53:02 +0800

    originalCallback could be undefined

--- src/ajax.js
+++ src/ajax.js
@@ -100,7 +100,7 @@
         ajaxSuccess(responseData[0], xhr, options, deferred)
       }
 
-      window[callbackName] = originalCallback
+      window[callbackName] = originalCallback || empty
       if (responseData && $.isFunction(originalCallback))
         originalCallback(responseData[0])
 
