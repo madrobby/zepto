9427658de2b9ef7bbcf899a4362106865ad3edd2
Mislav Marohnić <mislav.marohnic@gmail.com>
2016-07-13 00:31:42 +0200

    Fix assigning query params from data with explicit JSONP
    Fixes #884

--- src/ajax.js
+++ src/ajax.js
@@ -191,7 +191,7 @@
   function serializeData(options) {
     if (options.processData && options.data && $.type(options.data) != "string")
       options.data = $.param(options.data, options.traditional)
-    if (options.data && (!options.type || options.type.toUpperCase() == 'GET'))
+    if (options.data && (!options.type || options.type.toUpperCase() == 'GET' || 'jsonp' == options.dataType))
       options.url = appendQuery(options.url, options.data), options.data = undefined
   }
 
