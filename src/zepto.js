01f2682b1234655b02fe53ca0f1af015b89abcc1
Mislav Marohnić <mislav.marohnic@gmail.com>
2016-07-13 01:12:38 +0200

    Improve detection of array-like objects
    Previously, a hash with `length: 1` property would be falsely detected
    as array-like object. Adopt the implementation from jQuery for more
    robust "array-like" detection.
    
    Authors: Timmy Willison, Rick Waldron, Richard Gibson.
    
    This approach is still deterred by a hash such as `{ length: 0, a: "b" }`.
    jQuery 3.0.0 seems to suffer from the same problem.
    
    Fixes #918

--- src/zepto.js
+++ src/zepto.js
@@ -75,7 +75,16 @@ var Zepto = (function() {
   function isPlainObject(obj) {
     return isObject(obj) && !isWindow(obj) && Object.getPrototypeOf(obj) == Object.prototype
   }
-  function likeArray(obj) { return typeof obj.length == 'number' }
+
+  function likeArray(obj) {
+    var length = !!obj && 'length' in obj && obj.length,
+      type = $.type(obj)
+
+    return 'function' != type && !isWindow(obj) && (
+      'array' == type || length === 0 ||
+        (typeof length == 'number' && length > 0 && (length - 1) in obj)
+    )
+  }
 
   function compact(array) { return filter.call(array, function(item){ return item != null }) }
   function flatten(array) { return array.length > 0 ? $.fn.concat.apply([], array) : array }
