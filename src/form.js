cbb5ae219ca652f8c5221f937975dbb24565bec2
jamesanthony <sunqijun1523@gmail.com>
2016-12-27 15:10:23 +0800

    use 'on' instead

--- src/form.js
+++ src/form.js
@@ -28,7 +28,7 @@
   }
 
   $.fn.submit = function(callback) {
-    if (0 in arguments) this.bind('submit', callback)
+    if (callback && typeof callback === 'function') this.on('submit', callback)
     else if (this.length) {
       var event = $.Event('submit')
       this.eq(0).trigger(event)
