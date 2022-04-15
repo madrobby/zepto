64084b111e34760b750369dd5fef883a66781fc9
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-15 10:23:13 +0800

    Delete src directory

--- src/amd_layout.js
+++
@@ -1,9 +0,0 @@
-(function(global, factory) {
-  if (typeof define === 'function' && define.amd)
-    define(function() { return factory(global) })
-  else
-    factory(global)
-}(window, function(window) {
-  YIELD
-  return Zepto
-}))
